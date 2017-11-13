f18d3e93ab12fee562371ca5989100d6caabe5e9
Shinnosuke Ooyama <shinnosuke_oyama@citycom.co.jp>
2017-11-13 16:59:21 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let http = require('http')
+let createHandler = require('github-webhook-handler')
+let secret = process.env.SECRET;
+let handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
+
+if (!secret) throw new Error('environment SECRET is not defined')
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(7777)
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+})
+
+handler.on('issues', function (event) {
+  console.log('Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title)
+})
